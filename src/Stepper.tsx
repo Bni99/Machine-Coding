edf297594dc19182a77d74cf739e907583595ad6
Akriti Sood <soodakriti76@gmail.com>
2025-03-09 11:46:36 +0530

    Stepper - add remaining functionality

--- src/Stepper.tsx
+++ src/Stepper.tsx
@@ -14,23 +14,34 @@ const Stepper = () => {
       <div className="flex gap-8 h-[30vh] justify-center items-center">
         {steps.map((step, i) => {
           return (
-            <div className="flex flex-col items-center w-56 h-32" key={i}>
+            <div className="flex flex-col w-56 h-32 relative" key={i}>
               <div
                 className={`h-10 w-10 rounded-full border-1 border-gray-400 flex items-center justify-center ${
                   Number(i) == Number(stepper)
+                    ? "bg-blue-600 text-white border-none"
+                    : Number(i) < Number(stepper)
                     ? "bg-green-600 text-white border-none"
-                    : ""
+                    : null
                 }`}
               >
-                {i + 1}
+                {Number(i) < Number(stepper) ? "✔️" : i + 1}
               </div>
+              {i < 3 && (
+                <div
+                  className={`h-2 w-[217px] bg-gray-200 absolute top-4 left-10 ${
+                    Number(i) < Number(stepper)
+                      ? "bg-green-600 text-white border-none"
+                      : null
+                  }`}
+                ></div>
+              )}
               <div className="p-6">{step.name}</div>
             </div>
           );
         })}
       </div>
       <div className="flex justify-center items-center h-[10vh]">
-        {steps[stepper]?.description || ""}
+        {steps[stepper]?.description || "Your order has been delivered"}
       </div>
 
       <div className="flex justify-center items-center h-[10vh]">
@@ -41,7 +52,7 @@ const Stepper = () => {
               setStepper((stepper) => stepper + 1);
             }}
           >
-            Next
+            {stepper === 3 ? "Finish" : "Next"}
           </button>
         )}
       </div>
